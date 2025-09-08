4256f4cbecc09e47ca3146db75a2d82b9c1a51dd
CodeX93 <aghashahhyder@gmail.com>
2025-09-08 19:44:08 +0500

    v1.0-fixes2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ command, mode }) => ({
   base: "/",
@@ -23,7 +22,6 @@ export default defineConfig(({ command, mode }) => ({
   },
   plugins: [
     react(),
-    ...(command === "serve" ? [componentTagger()] : []), // only in dev server, never in build
   ],
   
   resolve: {
